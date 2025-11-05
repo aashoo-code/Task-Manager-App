094fd647a69438ce504596ab8d85f6fc0af5b532
Ashu <140695297+aashoo-code@users.noreply.github.com>
2025-11-05 21:55:24 +0530

    Create App component with routing setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { Route } from 'react-router-dom'
+import './App.css'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+import Home from './Pages/Home'
+import { Routes,Navigate } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Navigate to='/login' />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
